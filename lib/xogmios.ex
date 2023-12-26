c6e4aaa1be76101f4eb5cb46a4ff922c144bf4fc
Carlos Souza <carloshrsouza@gmail.com>
2023-12-26 06:19:51 -0500

    New Elixir 1.16 mix project

---
+++ lib/xogmios.ex
@@ -0,0 +1,18 @@
+defmodule Xogmios do
+  @moduledoc """
+  Documentation for `Xogmios`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Xogmios.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/xogmios_test.exs
@@ -0,0 +1,8 @@
+defmodule XogmiosTest do
+  use ExUnit.Case
+  doctest Xogmios
+
+  test "greets the world" do
+    assert Xogmios.hello() == :world
+  end
+end
