f05ce474d79bb3fd0a94cab7c46bafedf32d3888
Carlos Souza <carloshrsouza@gmail.com>
2024-01-01 11:42:48 -0500

    Update README

--- lib/xogmios.ex
+++ lib/xogmios.ex
@@ -1,22 +1,2 @@
 defmodule Xogmios do
-  def chain_sync() do
-    pid = GenServer.whereis(Xogmios.ClientExampleA)
-    Xogmios.ClientExampleA.send_frame(pid, message())
-  end
-
-  def chain_sync_2() do
-    pid = GenServer.whereis(Xogmios.ClientExampleB)
-    Xogmios.ClientExampleB.send_frame(pid, message())
-  end
-
-  defp message do
-    # Syncs with tip
-    ~S"""
-    {
-      "jsonrpc": "2.0",
-      "method": "nextBlock",
-      "id": "start"
-    }
-    """
-  end
 end
