e3a303433f17fec39e005d28839ac221191f4ef3
Carlos Souza <carloshrsouza@gmail.com>
2024-08-03 17:17:02 -0400

    Remove unused messages

--- lib/xogmios/mempool/messages.ex
+++ lib/xogmios/mempool/messages.ex
@@ -43,33 +43,6 @@ defmodule Xogmios.Mempool.Messages do
     json
   end
 
-  def size_of_mempool() do
-    json = ~S"""
-    {
-      "jsonrpc": "2.0",
-      "method": "sizeOfMempool"
-    }
-    """
-
-    validate_json!(json)
-    json
-  end
-
-  def has_transaction(tx_id) do
-    json = ~s"""
-    {
-      "jsonrpc": "2.0",
-      "method": "hasTransaction",
-      "params": {
-        "id": "#{tx_id}"
-      }
-    }
-    """
-
-    validate_json!(json)
-    json
-  end
-
   defp validate_json!(json) do
     case Jason.decode(json) do
       {:ok, _decoded} -> :ok
