5f7c7cc8328ca1394c5a5ede69348e7371ebbc41
Carlos Souza <carloshrsouza@gmail.com>
2024-09-18 17:32:30 -0400

    Fix Ogmios connection error
    This error would take place when the Ogmios service is started
    with an invalid socket file. The Ogmios service is able to start and
    receive connections but the handshake is never completed and the client
    errors out when trying to send frames.
    
    The fix was added to the underlying websocket_client library and this
    commit simply updates Xogmios to point to the patched version.

--- mix.exs
+++ mix.exs
@@ -50,7 +50,7 @@ defmodule Xogmios.MixProject do
       {:mix_audit, "~> 2.1", only: [:dev, :test], runtime: false},
       {:plug, "~> 1.15", only: :test},
       {:plug_cowboy, "~> 2.6", only: :test},
-      {:websocket_client, "~> 1.5"}
+      {:websocket_client, github: "caike/websocket_client", ref: "2ffd6c3"}
     ]
   end
 
