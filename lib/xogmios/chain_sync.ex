bc3e67f140faffda811143454eb0c2f10e41dd1f
Carlos Souza <carloshrsouza@gmail.com>
2025-06-16 15:53:22 -0400

    Add call function docs.

--- lib/xogmios/chain_sync.ex
+++ lib/xogmios/chain_sync.ex
@@ -146,6 +146,13 @@ defmodule Xogmios.ChainSync do
     end
   end
 
+  @doc """
+  Sends a message to the ChainSync process.
+
+  This function is useful to send messages to the ChainSync process from outside
+  of the ChainSync module. The message should be handled by a matching `c:handle_info/2` callback.
+  """
+  @spec call(pid(), term()) :: {:ok, term()} | {:error, term()}
   def call(pid, message) do
     case get_ws_pid(pid) do
       {:ok, ws_pid} ->
